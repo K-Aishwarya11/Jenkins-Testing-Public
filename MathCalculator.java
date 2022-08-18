3904a81a28d644f03f98d346522bb73ed2f191cc
Aishwarya Katkar <katkaraishwarya11@gmail.com>
2022-08-18 22:30:24 +0530

    MathCalculator Added

---
+++ MathCalculator.java
@@ -0,0 +1,36 @@
+
+public class MathCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MathCalculator math = new MathCalculator();
+		
+		math.Addition();
+		math.Subtraction();
+		math.Multiplication();
+		math.Division();
+
+	}
+	
+	public void Addition()
+	{
+		System.out.println("I am Addition");
+	}
+	
+	public void Subtraction()
+	{
+		System.out.println("I am Subtraction");
+	}
+	
+	public void Multiplication()
+	{
+		System.out.println("I am Multiplication");
+	}
+	
+	public void Division()
+	{
+		System.out.println("I am Division");
+	}
+
+}
